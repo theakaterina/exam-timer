7f6d812dcb254d7010093d6686157fda67d4362f
KOUTSOUKIS <tkout0@eq.edu.au>
2021-09-27 20:38:19 +1000

    Removed dependency on setInterval/Timeout for keeping time (not accurate

--- src/Timer.js
+++ src/Timer.js
@@ -6,62 +6,87 @@ import React, { useEffect, useState, useRef } from "react";
  * @param {string} props.minutes
  */
 
-/*Timer has minutes as input*/
+/* Timer has minutes as input*/
 const Timer = (props) => {
-  /*Start by converting minutes to seconds */
-  const [globalSeconds, setSeconds] = useState(Number(props.minutes) * 60);
   const [isActive, setIsActive] = useState(true);
   const [intervalId, setIntervalId] = useState();
-  const isMounted = useRef(true);
+  const [startTime, setStartTime] = useState(Date.now());
+  const [elapsedTime, setElapsedTime] = useState(0);
 
-  /*Using total seconds, find the hours, minutes and seconds left*/
-  const calculateTime = (seconds) => {
-    const totalMinutes = Math.floor(seconds / 60);
-    const hours = String(Math.floor(totalMinutes / 60));
-    const minutes =
-      hours === "0"
-        ? String(totalMinutes % 60)
-        : String(totalMinutes % 60).padStart(2, "0");
-    const localSeconds =
+  /* Convert minutes to milliseconds */
+  const finishTime = Number(props.minutes) * 60 * 1000;
+
+  /* Using milliseconds left, find the hours, minutes and seconds left */
+  const calculateTime = (ms) => {
+    const seconds = ms / 1000;
+    const minutes = Math.floor(seconds / 60);
+    const displayHours = String(Math.floor(minutes / 60));
+    const displayMinutes =
+      displayHours === "0"
+        ? String(minutes % 60)
+        : String(minutes % 60).padStart(2, "0");
+    const displaySeconds =
       seconds < 60
-        ? String(seconds % 60)
-        : String(seconds % 60).padStart(2, "0");
-    return [hours, minutes, localSeconds];
+        ? String(Math.floor(seconds % 60))
+        : String(Math.floor(seconds % 60)).padStart(2, "0");
+    return [displayHours, displayMinutes, displaySeconds];
   };
 
-  /*Pause timer*/
+  /* Pause timer */
   function toggle() {
     setIsActive((f) => !f);
   }
 
-  /*Restart timer*/
+  /* Restart timer */
   function reset() {
     setIsActive(false);
-    setSeconds(Number(props.minutes) * 60);
+    setElapsedTime(0);
   }
 
-  /*While active, every second, decrease the number of seconds by 1*/
+  /* setInterval and setTimeout are not accurate - they can be off by several
+     milliseconds, so we need to keep track of time using Date.now().
+   */
+  const updateElapsedTime = () => {
+    const now = Date.now();
+    const timeSinceLastTick = now - startTime;
+    setElapsedTime((s) => s + timeSinceLastTick);
+    setStartTime((s) => now);
+  }
+
+  /* If it's paused, keep updating elapsedTime, when it gets unpaused set the
+     startTime to current time
+  */
   useEffect(() => {
-    /*isMounted is necessary to stop memory leaks when the first timer hits 0*/
-    isMounted.current = true;
+    if (!isActive) {
+      updateElapsedTime();
+    } else {
+      setStartTime(Date.now());
+    }
+  }, [isActive]);
+
+  /* Mostly react stuff that I don't really understand...
+     Updates the elapsedTime and startTime roughly very 500ms
+  */
+  useEffect(() => {
     if (!isActive && intervalId) {
       clearTimeout(intervalId);
+      setIntervalId(null);
     } else if (isActive) {
-      setIntervalId(
-        setTimeout(() => {
-          if (isMounted.current) {
-            setSeconds((s) => s - 1);
-          }
-        }, 1000)
-      );
+      if (intervalId) {
+        clearTimeout(intervalId);
+        setIntervalId(null);
+      }
+      setIntervalId(setTimeout(() => {
+        updateElapsedTime();
+      }, 500));
     }
     return () => {
-      clearTimeout(intervalId);
-      isMounted.current = false;
-    };
-  }, [isActive, globalSeconds]);
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }, [isActive, startTime]);
 
-  /*Listen for the pause/start and restart buttons*/
+  /* Listen for the pause/start and restart buttons */
   useEffect(() => {
     window.addEventListener("toggleTimer", toggle);
     window.addEventListener("resetTimer", reset);
@@ -71,17 +96,20 @@ const Timer = (props) => {
     };
   }, []);
 
-  /*Once the timer reaches 0, turn off*/
+  /* Once the timer reaches 0, turn off */
   useEffect(() => {
-    if (globalSeconds <= 0) {
+    if (elapsedTime >= finishTime) {
       setIsActive(false);
+      /* Set elapsedTime otherwise final time shown will be negative */
+      setElapsedTime(finishTime);
       props.onEnd();
     }
-  }, [globalSeconds]);
+  }, [elapsedTime]);
 
-  const time = calculateTime(globalSeconds);
+  /* Time left */
+  const time = calculateTime(finishTime - elapsedTime);
 
-  /*Only display the hours and minutes if there are hours and minutes left*/
+  /* Only display the hours and minutes if there are hours and minutes left */
   return (
     <div className="timer">
       {time[0] === "0" ? "" : time[0]}
